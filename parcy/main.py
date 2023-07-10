2ddc8b381f8e434f37c4d3dd3efbfb598a282c48
nvitucci <nicola.vitucci@gmail.com>
2023-07-10 22:38:29 +0100

    Update README

--- parcy/main.py
+++ parcy/main.py
@@ -1,19 +1,27 @@
 import dataclasses
 import json
+import sys
 
 from parser import Parser
 
 
-query = """
-    MATCH (n:Person)-[r:KNOWS]->(m:Person)-[:GOES|:WALKS]->(p:Place)
-    WHERE n.name = 'Alice' AND m:Person
-    RETURN *, n, m.name AS nn, p
-    ORDER BY n DESC
-    SKIP 10
-    LIMIT 1
-"""
+# TODO: make this more flexible and robust
+if __name__ == "__main__":
+    args = sys.argv[1:]
 
-parser = Parser(query)
+    if len(args):
+        query = args[0]
+    else:
+        query = """
+            MATCH (n:Person)-[r:KNOWS]->(m:Person)-[:GOES|:WALKS]->(p:Place)
+            WHERE n.name = 'Alice' AND m:Person
+            RETURN *, n, m.name AS nn, p
+            ORDER BY n DESC
+            SKIP 10
+            LIMIT 1
+        """
 
-print(parser.query_object)
-print(json.dumps(dataclasses.asdict(parser.query_object), indent=2))
+    parser = Parser(query)
+
+    print(parser.query_object)
+    print(json.dumps(dataclasses.asdict(parser.query_object), indent=2))
